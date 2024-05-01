42aeb9b7ef48512daf59d597ea27e2992b66eef9
DeclanGH <declanoonunkwo@gmail.com>
2024-04-30 21:37:13 -0400

    delete files

--- src/main/java/io/github/declangh/sharedtexteditor/WebSocketTextHandler.java
+++
@@ -1,6 +0,0 @@
-package io.github.declangh.sharedtexteditor;
-
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-
