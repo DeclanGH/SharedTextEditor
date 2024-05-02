d07fcde6399c45c80271d1636dd982215e3c81db
DeclanGH <declanoonunkwo@gmail.com>
2024-05-02 14:03:17 -0400

    remove all textMap code

--- src/main/java/io/github/declangh/sharedtexteditor/EditorClient.java
+++ src/main/java/io/github/declangh/sharedtexteditor/EditorClient.java
@@ -56,8 +56,6 @@ public class EditorClient extends JFrame {
                     String insertedText;
                     try {
                         insertedText = event.getDocument().getText(event.getOffset(), event.getLength());
-                        // update map
-                        addOperation(event.getOffset(), event.getLength(), insertedText);
 
                         // After doing the operation locally, get the packet to broadcast it out
                         byte[] insertPacket = Packets.createInsertPacket(event.getOffset(), event.getLength(), insertedText);
@@ -81,9 +79,6 @@ public class EditorClient extends JFrame {
                     // String removedText;
                     int offset = event.getOffset();
                     int length = event.getLength();
-
-                    // update map
-                    deleteOperation(offset, length);
 
                     // After doing the operation locally, get the packet to broadcast it out
                     byte[] deletePacket = Packets.createDeletePacket(event.getOffset(), event.getLength());
@@ -148,107 +143,6 @@ public class EditorClient extends JFrame {
         }
     }
 
-    // This method is for updating the hashmap for which characters map to which position.
-    // This should be called after every update in the text editor.
-    private static void addOperation(int offset, int length, String newCharacter){
-        char value;
-        int key;
-        // First check to see if there is a key for that offset
-        if(textMap.containsKey(offset)){
-            // Make a new map
-            HashMap<Integer, Character> tempMap = new HashMap<>();
-            
-            // Make a new map and map each value up to the offset to the same position
-            for(int i = 0; i < textMap.size(); i++){
-                
-                // If it is less than the offset (the new chars position) then you can just put in the new map
-                if(i < offset){
-                    value = textMap.get(i);
-                    tempMap.put(i, value);
-                }
-
-                // if i = the offset, put the new character at that position and the old character at the position + length
-                else if(i == offset){
-                    // Check if the length is greater than 1
-                    if(length > 1){
-                        // If it is, we need a FOR loop to add each character in the string to the map and move the old characters to the new place
-                        for(int j = 0; j < length; j++){
-                            tempMap.put(i + j, newCharacter.charAt(j));
-                            value = textMap.get(i + j);
-                            tempMap.put(i + length, value);
-                        }
-                    }
-                    // Else just replace the one character
-                    else{
-                        tempMap.put(i, newCharacter.charAt(0));
-                        value = textMap.get(i);
-                        tempMap.put(i+1, value);
-                    }
-                }
-                // Else, map to the new position (oldPosition + length)
-                else{
-                    key = i + length;
-                    value = textMap.get(i);
-                    tempMap.put(key, value);
-                }
-            }
-            // Clear the old map and copy the new map
-            textMap.clear();
-            textMap.putAll(tempMap);
-            System.out.println(tempMap.entrySet() + " temp map");
-        } else{
-            // Check to see the length of the new text
-            if (length > 1){
-                for(int i = 0; i < length; i++){
-                    textMap.put(offset + i, newCharacter.charAt(i));
-                }
-            } else{
-                // If there isnt, put the new character at that position
-                textMap.put(offset, newCharacter.charAt(0));
-            }
-        }
-    }
-
-    // This is called when a delete operation occurs.
-    private static void deleteOperation(int offset, int length){
-        char value;
-        int key; 
-
-        // If the offset = size of the map
-        if(offset == textMap.size()){
-            // This means the offset is the last character in the map, so no need to update others
-            textMap.remove(offset);
-        }
-        // Else there are characters that need to be moved
-        else{
-            // Make a new map
-            HashMap<Integer, Character> tempMap = new HashMap<Integer, Character>();
-
-            // Make a new map and map each value up to the offset to the same position
-            for(int i = 0; i < textMap.size(); i++){
-    
-                // If it is less than the offset then you can just put it in the new map
-                if(i < offset){
-                    value = textMap.get(i);
-                    tempMap.put(i, value);
-                }
-                // if i = the offset, then continue. We dont want that value
-                else if(i == offset || i < offset + length){
-                    continue;
-                }
-                // Else, map to the new position (oldPosition - length)
-                else{
-                    key = i - length;
-                    value = textMap.get(i);
-                    tempMap.put(key, value);
-                }
-            }
-            //Clear the old map and copy the new map
-            textMap.clear();
-            textMap.putAll(tempMap);
-        }
-    }
-
     public static void receivePacket(byte[] packet){
 
         // If we are receiving a packet, we are about to get an external update
@@ -290,11 +184,6 @@ public class EditorClient extends JFrame {
             textArea.getDocument().remove(offset,length);
         } catch (BadLocationException e) {
             throw new RuntimeException(e);
-        }
-
-        // Loop through Map from offset to length, deleting each character in the text area
-        for(int i = offset; i < length; i++){
-            textArea.remove(i);
         }
     }
 
