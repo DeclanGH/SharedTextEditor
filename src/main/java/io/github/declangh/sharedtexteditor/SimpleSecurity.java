d069984e469e5da31cc4885b35d97df1147d638d
DeclanGH <declanoonunkwo@gmail.com>
2024-05-06 00:50:13 -0400

    add code to skip encrypting/decryption operation ordinal

--- src/main/java/io/github/declangh/sharedtexteditor/SimpleSecurity.java
+++ src/main/java/io/github/declangh/sharedtexteditor/SimpleSecurity.java
@@ -10,9 +10,17 @@ public class SimpleSecurity {
         return r;
     }
 
+    // this would be the starting iteration of encryption. We want to ignore the Operation Ordinal
+    private static final int START_ITR = Integer.BYTES;
+
     public static byte[] encrypt(byte[] byteArray, long key) {
 
-        for (int i=0; i<byteArray.length; i++) {
+        if (byteArray.length <= START_ITR) {
+            System.out.println("First four bytes are assumed to be Operation Ordinal.");
+            throw new IllegalArgumentException("Byte array must be at least 5 bytes long!");
+        }
+
+        for (int i=START_ITR; i<byteArray.length; i++) {
             byteArray[i] = (byte) (byteArray[i] ^ (key & 0xFF));
         }
 
@@ -21,7 +29,12 @@ public class SimpleSecurity {
 
     public static byte[] decrypt(byte[] byteArray, long key) {
 
-        for (int i=0; i<byteArray.length; i++) {
+        if (byteArray.length <= START_ITR) {
+            System.out.println("First four bytes are assumed to be Operation Ordinal.");
+            throw new IllegalArgumentException("Byte array must be at least 5 bytes long!");
+        }
+
+        for (int i=START_ITR; i<byteArray.length; i++) {
             byteArray[i] = (byte) (byteArray[i] ^ (key & 0xFF));
         }
 
